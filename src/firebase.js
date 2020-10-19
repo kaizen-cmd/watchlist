22c0c465c682c446c1d02d67b26facd202761f88
Tejas Mandre <tejasmandre0306@gmail.com>
2020-10-19 17:59:24 +0530

    creds of firebase added

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyByxs05WZBWq9Gs1E1Lae3iXRV8hPckg-I",
+  authDomain: "watchlist-5c02c.firebaseapp.com",
+  databaseURL: "https://watchlist-5c02c.firebaseio.com",
+  projectId: "watchlist-5c02c",
+  storageBucket: "watchlist-5c02c.appspot.com",
+  messagingSenderId: "565445319709",
+  appId: "1:565445319709:web:2065bc890868b8e12b7af4",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
